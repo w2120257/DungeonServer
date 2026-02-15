6fd35815edb320a25ea7b0e9621e0ff0f8357fb5
w2120257 <sandupama.20231385@iit.ac.lk>
2026-02-15 20:07:18 +0530

    Initiated the application

---
+++ src/main/java/com/example/dungeonserver/DungeonServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.dungeonserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DungeonServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DungeonServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dungeonserver/DungeonServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dungeonserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DungeonServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
