64926fc615de47f5e258c97d518b86655867231f
w2120257 <sandupama.20231385@iit.ac.lk>
2026-02-16 00:56:57 +0530

    API: Added PlayerController to handle web requests

---
+++ src/main/java/com/example/dungeonserver/controller/PlayerController.java
@@ -0,0 +1,28 @@
+package com.example.dungeonserver.controller;
+
+import com.example.dungeonserver.model.Player;
+import com.example.dungeonserver.service.PlayerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/player")
+public class PlayerController {
+
+    @Autowired
+    private PlayerService playerService;
+
+    // We use @GetMapping here so we can test it easily in our browser...
+    // Usually this would be @PostMapping, but browsers love GET.
+    // URL: http://localhost:8080/api/player/spawn?name=HeroName
+    @GetMapping("/spawn")
+    public Player spawn(@RequestParam String name) {
+        return playerService.spawnPlayer(name);
+    }
+
+    // URL: http://localhost:8080/api/player/1
+    @GetMapping("/{id}")
+    public Player getPlayer(@PathVariable Long id) {
+        return playerService.getPlayer(id);
+    }
+}
