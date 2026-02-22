c229732a7643dd8e29e46746426488637489fc23
w2120257 <sandupama.20231385@iit.ac.lk>
2026-02-22 22:31:42 +0530

    updated playerService class

--- src/main/java/com/example/dungeonserver/service/PlayerService.java
+++ src/main/java/com/example/dungeonserver/service/PlayerService.java
@@ -1,5 +1,6 @@
 package com.example.dungeonserver.service;
 
+import com.example.dungeonserver.model.Item;
 import com.example.dungeonserver.model.Player;
 import com.example.dungeonserver.repository.PlayerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,18 +16,29 @@ public class PlayerService {
         Player player = new Player();
         player.setName(name);
 
-        // Game Logic: Roll for stats (Like D&D)
-        // Base Strength is 10, plus a random roll of 0-5
+        // Game Logic: Roll for stats
         int bonusStr = (int)(Math.random() * 5);
         player.setStrength(10 + bonusStr);
 
-        // Base Defense is 5, plus a random roll of 0-3
         int bonusDef = (int)(Math.random() * 3);
         player.setDefense(5 + bonusDef);
 
-        // Give them starter gold
         player.setGold(50);
 
+        // --- NEW: STARTER LOOT ---
+        Item starterWeapon = new Item();
+        starterWeapon.setName("Rusty Sword");
+        starterWeapon.setType("WEAPON");
+        starterWeapon.setPower(3);
+        starterWeapon.setValue(5);
+
+        // Link the item to the player
+        starterWeapon.setPlayer(player);
+
+        // Put the item in the player's inventory
+        player.getInventory().add(starterWeapon);
+
+        // Save the player (This will automatically save the item too because of CascadeType.ALL)
         return playerRepository.save(player);
     }
 
